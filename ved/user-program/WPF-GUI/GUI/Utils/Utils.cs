44a333d1e5451c47bd00f03bfb4d09aad43991a6
dumitory <80849857+dumitory@users.noreply.github.com>
2021-07-09 13:31:16 +0300

    codestyle: remove unused packages

--- ved/user-program/WPF-GUI/GUI/Utils/Utils.cs
+++ ved/user-program/WPF-GUI/GUI/Utils/Utils.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Security.Principal;
+
 namespace GUI.Utils
 {
     static class Utils
